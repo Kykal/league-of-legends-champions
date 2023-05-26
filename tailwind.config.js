0ca5b053305028640ccac8fa501f885e7f6ebef4
Kykal <benavides.alan@hotmail.com>
2023-05-26 07:26:27 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,22 @@ module.exports = {
   theme: {
     extend: {
       textColor: {
-        'white': '#E6E6E6',
+        'custom-white': '#E6E6E6',
         'light-gold': '#C9AA71',
         'gold': '#CAAB72',
         'dark-gold': '#C19144',
+        'dimmed': '#888888',
       },
       backgroundColor: {
         'ultra-light-blue': '#2C70B5',
         'light-blue': '#092949',
         'blue': '#061F37',
         'dark-blue': '#060D1E',
+      },
+      'borderColor': {
+        'light-gold': '#C9AA71',
+        'gold': '#CAAB72',
+        'dark-gold': '#C19144',
       },
     },
   },
